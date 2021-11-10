f2e049e3283ce482503cd7e88b097e930d124a0f
HoangNgocNam <hoangngocnam2310@gmail.com>
2021-11-10 08:54:22 +0700

    update

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php
+$tuderts = array(
+    array('name'=>'tiep','age'=>18,'address'=>'ba tu'),
+    array('name'=>'nam','age'=>30,'address'=>'ha noi'),
+    array('name'=>'luc','age'=>20,'address'=>'dubai'),
+    array('name'=>'ha thu','age'=>16,'address'=>'phu tho'),
+    array('name'=>'dung','age'=>17,'address'=>'hue'),
+);
